8006ab439b41b5f8be73e5c737cdfe7d137aa8fb
aswanthmc <mcaswanth5@gmail.com>
2024-12-12 20:42:01 +0530

    trying to connecting data base

---
+++ dbconfig.js
@@ -0,0 +1,5 @@
+require("dotenv").config();
+const{pool}=require ("pg")
+const is production=process.env;
+Node_Env==="production";
+const connectionString='postgresql://${process.env.DB_USER}:${process.env.DB_USER}@${process.env.DB_USER}@${process.env.DB_USER}:${process.env.DB_USER}'
