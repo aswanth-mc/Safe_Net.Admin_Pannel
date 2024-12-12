e3305ef9ed4e2e6cf67e958469324641fdc52551
aswanthmc <mcaswanth5@gmail.com>
2024-12-12 19:05:01 +0530

    update js file to add the login and html pages to the server

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const express=require('express');
 const app= express();
 const PORT=process.env.PORT || 4000;
+const path=require('path')
 
 app.get('/' ,(req,res) => {
-    res.send("hello")
+    res.sendFile(path.join(__dirname,'views','login.html'));
+});
+app.get('/home' ,(req,res) => {
+    res.sendFile(path.join(__dirname,'views','home.html'));
 });
 
 
