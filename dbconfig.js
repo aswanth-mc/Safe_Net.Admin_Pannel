7047c09189dd7109b4f974963386911005704446
aswanthmc <mcaswanth5@gmail.com>
2024-12-13 18:33:23 +0530

    delete .env and dbconfig file to change postgrees into mongodb

--- dbconfig.js
+++
@@ -1,5 +0,0 @@
-require("dotenv").config();
-const{pool}=require ("pg")
-const is production=process.env;
-Node_Env==="production";
-const connectionString='postgresql://${process.env.DB_USER}:${process.env.DB_USER}@${process.env.DB_USER}@${process.env.DB_USER}:${process.env.DB_USER}'
